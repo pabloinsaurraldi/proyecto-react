ba68f277a9aa4e87f40d1274f6fb94fe4f354e27
Pablo Insaurraldi <pabloinsaurraldi@gmail.com>
2022-12-20 03:06:34 -0300

    agregado logo carrito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/containers/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer />
     </div>
   );
 }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import '../NavBar/styles.scss'
+import '../NavBar/styles.scss';
+
 
 const NavBar = () => {
   return (
    <nav className="navbar navbar-expand-lg colorNavBar">
   <div className="container-fluid">
-    <a className="navbar-brand  controlPadding" href="/#">Navbar</a>
+    <a className="navbar-brand  controlPadding" href="/#"><img src="" alt="" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,16 +16,13 @@ const NavBar = () => {
           <a className="nav-link active colorEnlaces" aria-current="page" href="/#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="colorEnlaces" href="/#">Link</a>
+          <a className="colorEnlaces" href="/#">Productos</a>
         </li>
         <li className="nav-item">
-          <a className="colorEnlaces" href="/#">Link</a>
+          <a className="colorEnlaces" href="/#">Contactanos</a>
         </li>
         <li className="nav-item">
-          <a className="colorEnlaces" href="/#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="colorEnlaces" href="/#">Link</a>
+          <a className="colorEnlaces" href="/#">Acerca de nosotros</a>
         </li>
        
       </ul>
@@ -34,6 +32,7 @@ const NavBar = () => {
       </form>
     </div>
   </div>
+  <img className='carrito' src="https://cdn-icons-png.flaticon.com/512/46/46297.png?w=360" alt="imgCart" />
 </nav>
   )
 }
