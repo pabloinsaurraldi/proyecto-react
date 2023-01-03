724bac11030c8df59bb82d82b9ed224044c56571
Pablo Insaurraldi <pabloinsaurraldi@gmail.com>
2023-01-03 01:30:29 -0300

    ya se muestra cant en el dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/containers/ItemListContainer';
+import ItemListContainer from './components/containers/ItemListContainer'
+import Cantidad from './components/conteo/index'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer />
+      <Cantidad />
     </div>
   );
 }
